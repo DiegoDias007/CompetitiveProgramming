f1352f7b38f027751268c04d7f5f950e54cca71e
Diego <diegoddias007@gmail.com>
2025-08-02 18:02:57 -0300

    Edit Distance

---
+++ cses/dynamic programming/Edit Distance.cpp
@@ -0,0 +1,47 @@
+// https://cses.fi/problemset/task/1639
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+typedef long long ll;
+using namespace std;
+
+template <typename T>T gcd(T a, T b) {if (b == 0) return a; return gcd(b, a % b);}
+template <typename T> T lcm(T a, T b) {return (a * b) / gcd(a, b);}
+template <typename K> void print_vec(const vector<K>& vec) {for(size_t i = 0; i < vec.size(); ++i) {cout << vec[i];if(i != vec.size() - 1) {cout << " ";}}cout << endl;}
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    int n = s.size(), m = t.size();
+    s = "&" + s;
+    t = "&" + t;
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 1e9));
+    for (int i = 0; i <= n; i++) dp[i][0] = i;
+    for (int i = 0; i <= m; i++) dp[0][i] = i;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i] == t[j]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1);
+            dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+            dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+        }
+    }
+    cout << dp[n][m] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
