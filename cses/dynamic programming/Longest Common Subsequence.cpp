40c17efcd79bf9834804dfff6a2d51859649d81f
Diego <diegoddias007@gmail.com>
2025-08-03 16:48:23 -0300

    Longest Common Subsequence

---
+++ cses/dynamic programming/Longest Common Subsequence.cpp
@@ -0,0 +1,68 @@
+// https://cses.fi/problemset/task/3403/
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+typedef long long ll;
+using namespace std;
+
+template <typename T>T gcd(T a, T b) {if (b == 0) return a; return gcd(b, a % b);}
+template <typename T> T lcm(T a, T b) {return (a * b) / gcd(a, b);}
+template <typename K> void print_vec(const vector<K>& vec) {for(size_t i = 0; i < vec.size(); ++i) {cout << vec[i];if(i != vec.size() - 1) {cout << " ";}}cout << endl;}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n + 1), b(m + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i <= m; i++) {
+        cin >> b[i];
+    }
+
+    // dp[i][j]: size of the longest common subsequence
+    // when we use the first i elements of array a and
+    // the first j elements of array b
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+            dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+            if (a[i] == b[j]) {
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+            }
+        }
+    }
+
+    vector<int> ans;
+    int i = n, j = m;
+    while (i > 0 && j > 0) {
+        if (a[i] == b[j]) {
+            ans.push_back(a[i]);
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << dp[n][m] << endl;
+    print_vec(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
